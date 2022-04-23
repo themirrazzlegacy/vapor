b2f2fc2310f83184e85f13505231dd6b183712f4
themirrazz <89490659+themirrazz@users.noreply.github.com>
2022-04-23 12:55:33 -0700

    Update Feed.js

--- Feed.js
+++ Feed.js
@@ -14,6 +14,26 @@ function onFeedAdded(call,f$) {
   });
 }
 
+function logStateChanges(call) {
+  console.log(call.state);
+  call.on('state', function () {
+    console.log(call.state)
+  });
+}
+
+function onFeedRemoved(call,f$) {
+  var feeds = getRemoteFeeds(call);
+  call.on('feeds_changed', function (e) {
+    var newfeeds=getRemoteFeeds(call);
+    for(var i=0;i<feeds.length;i++) {
+      if(newfeeds.indexOf(feeds[i])<0) {
+        f$(feeds[i])
+      }
+    }
+    feeds=newfeeds;
+  });
+}
+
 function getRemoteFeeds(call) {
   var feeds=[];
   var f$=call.getFeeds();
